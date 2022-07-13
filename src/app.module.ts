68c1cca981a4a028dc1607a81dc37fb4d7a78e23
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-13 15:08:57 +0900

    tyoeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { HttpLogger } from './middleware/http-logger.middleware';
 import { KakaoMiddleware } from './middleware/kakao.middleware';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -20,6 +21,17 @@ import * as Joi from 'joi';
         DB_PASSWORD: Joi.string().required(),
       }),
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
