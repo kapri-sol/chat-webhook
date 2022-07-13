ed2e8e5e1f439d201ea324e17fcb77717567499e
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-13 15:08:41 +0900

    config module 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpLogger } from './middleware/http-logger.middleware';
 import { KakaoMiddleware } from './middleware/kakao.middleware';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.valid('production', 'development', 'test'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
+    }),
   controllers: [AppController],
   providers: [AppService],
 })
