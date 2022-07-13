228324067b6cf764f6c1becc65143276d6e84ce4
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-13 15:47:07 +0900

    middleware에 유저 확인

--- src/middleware/kakao.middleware.ts
+++ src/middleware/kakao.middleware.ts
@@ -1,14 +1,35 @@
 import { NestMiddleware } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { NextFunction, Request, Response } from 'express';
+import { User } from 'src/entity/user.entity';
 import { KakaoDTO } from 'src/kakao/kakao.dto';
+import { Repository } from 'typeorm';
 
 export class KakaoMiddleware implements NestMiddleware {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   async use(req: Request, res: Response, next: NextFunction) {
     const kakao: KakaoDTO = req.body;
 
     if (!(kakao?.bot?.id && kakao?.intent?.id && kakao.userRequest.user.id)) {
       next();
       return;
+    }
+
+    let user = await this.userRepository.findOne({
+      where: {
+        kakaoUserId: kakao.userRequest.user.id,
+      },
+    });
+
+    if (!user) {
+      user = this.userRepository.create({
+        kakaoUserId: kakao.userRequest.user.id,
+      });
+
+      await this.userRepository.save(user);
     }
 
     // 경로 라우팅
@@ -19,6 +40,7 @@ export class KakaoMiddleware implements NestMiddleware {
       utterance: kakao.userRequest.utterance,
       isFriend: kakao.userRequest.user.properties?.isFriend || false,
       extra: kakao.action.clientExtra,
+      user,
     };
 
     next();
