17c582ae9afc3f3300b879760888fd32e36fab0c
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 14:59:09 +0900

    인증숫자 생성 함수 추가

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import * as crypto from 'crypto';
+import { AUTH_NUMBER_CACHE_KEY, AUTH_NUMBER_EXPIRE_TIME, AUTH_NUMBER_LENGTH } from 'src/constant/auth.constant';
 import * as sgMail from '@sendgrid/mail';
 import { ConfigService } from '@nestjs/config';
 import { Cache } from 'cache-manager';
@@ -10,5 +12,10 @@ export class AuthService {
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {
     sgMail.setApiKey(this.configService.get<string>('MAIL_API_KEY'));
+  }
+
+  generateAuthNumber(): string {
+    const maxNumber = parseInt('9'.repeat(AUTH_NUMBER_LENGTH));
+    return crypto.randomInt(maxNumber).toString();
   }
 }
