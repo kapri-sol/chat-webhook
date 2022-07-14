9987081d89bccae77b41f5f8107e64c94d33467e
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 14:57:23 +0900

    config service, cache manager 생성자 주입

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,5 +1,12 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+  ) {
+  }
 }
