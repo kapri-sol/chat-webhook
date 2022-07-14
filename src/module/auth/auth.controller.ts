23e920396996cfc27fc1b43c5ea806a8c220b886
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 18:18:55 +0900

    인증시간 초과 예외 처리

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -40,14 +40,47 @@ export class AuthController {
       user: { userUid },
     } = localData;
 
+    try {
     const isValid = await this.authService.validateAuthNumber(userUid, authNumber);
 
-    await this.userServicie.initRoute(userUid);
-
     if (isValid) {
-      return new KakaoReplyMaker().addSimpleText('인증되었습니다.').makeReply();
+        return new KakaoReplyMaker()
+          .addBasicCard({
+            description: '인증되었습니다!',
+            buttons: [
+              {
+                action: 'block',
+                label: '시작하기',
+                blockId: KakaoIntentId.WELCOME,
+              },
+            ],
+          })
+          .makeReply();
     } else {
-      return new KakaoReplyMaker().addSimpleText('인증에 실패했습니다.').makeReply();
+        return new KakaoReplyMaker().addSimpleText('인증이 실패했습니다.\n다시 입력해주세요.').makeReply();
+      }
+    } catch (err) {
+      if (err instanceof UnauthorizedException) {
+        return new KakaoReplyMaker()
+          .addBasicCard({
+            description: '인증이 가능한 시간이 지났습니다. .\n다시 인증해주세요',
+            buttons: [
+              {
+                action: 'block',
+                label: '다시 인증하기',
+                blockId: KakaoIntentId.START_AUTH,
+              },
+              {
+                action: 'block',
+                label: '처음으로',
+                blockId: KakaoIntentId.WELCOME,
+              },
+            ],
+          })
+          .makeReply();
+      }
+      throw err;
+    }
     }
   }
 }
