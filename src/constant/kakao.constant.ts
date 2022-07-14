bacff7fa368a8df45626110021a9b28fe7c39f06
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 18:19:28 +0900

    인증 메일 확인 블록 추가

--- src/constant/kakao.constant.ts
+++ src/constant/kakao.constant.ts
@@ -3,6 +3,7 @@ export enum KakaoIntentId {
   FALLBACK = '5ee886036fe05800015f6355',
 
   START_AUTH = '62ce6c03903c8b5a80049eec',
+  CHECK_AUTH = '62ce791de262a941bbdd0d09',
 }
 
 export enum KakaoFallbackRoute {
