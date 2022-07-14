a8640a79d37a10cb7de9b19b33917f929215db2c
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 18:20:01 +0900

    welcom, fallback 블록에 guard 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,49 @@
+import { UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { KakaoIntentId } from './constant/kakao.constant';
 import { KakaoController, KakaoIntent } from './decorator/kakao.decorator';
+import { AuthGuard } from './guard/auth.guard';
 import { KakaoReply, KakaoReplyMaker } from './kakao/kakao.type';
 
+@UseGuards(AuthGuard)
 @KakaoController()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @KakaoIntent(KakaoIntentId.WELCOME)
   welcome(): KakaoReply {
-    return new KakaoReplyMaker().addSimpleText('안녕하세요!').makeReply();
+    return new KakaoReplyMaker()
+      .addSimpleText('안녕하세요!')
+      .addBasicCard({
+        description: '안녕하세요!',
+        buttons: [
+          {
+            action: 'block',
+            label: '인증 확인하기',
+            blockId: KakaoIntentId.CHECK_AUTH,
+          },
+        ],
+      })
+      .makeReply();
   }
 
   @KakaoIntent(KakaoIntentId.FALLBACK)
-  getHello(): KakaoReply {
-    return new KakaoReplyMaker().addSimpleText('안녕하세요!').makeReply();
+  fallback(): KakaoReply {
+    return new KakaoReplyMaker()
+      .addBasicCard({
+        description: '폴백 답변입니다.',
+        buttons: [
+          {
+            action: 'block',
+            label: '시작',
+            blockId: KakaoIntentId.WELCOME,
+          },
+          {
+            action: 'block',
+            label: '인증 확인',
+            blockId: KakaoIntentId.CHECK_AUTH,
+          },
+        ],
+      })
+      .makeReply();
   }
 }
