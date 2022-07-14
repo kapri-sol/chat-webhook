42fcde7381c1f04d1dce0ce35583b9635e33413d
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 15:36:27 +0900

    exception filter 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ForbiddenExceptionFilter } from './filter/forbidden-exception.filter';
+import { InternalServerErrorFilter } from './filter/internal-server-error.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new ForbiddenExceptionFilter(), new InternalServerErrorFilter());
+
   await app.listen(3000);
 }
 bootstrap();
