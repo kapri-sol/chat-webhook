dfae73aad6d7ada04068452109f8d15524b36e4f
kapri <44130355+kapri-sol@users.noreply.github.com>
2022-07-14 15:16:09 +0900

    kakao intent decorator에 kakao fallback route 타입 추가

--- src/decorator/kakao.decorator.ts
+++ src/decorator/kakao.decorator.ts
@@ -1,19 +1,11 @@
-import {
-  applyDecorators,
-  Controller,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import { KakaoIntentId } from 'src/constant/kakao.constant';
+import { applyDecorators, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { KakaoFallbackRoute, KakaoIntentId } from 'src/constant/kakao.constant';
 import { KakaoInterceptor } from 'src/interceptor/kakao.interceptor';
 
 export const KakaoController = () => {
-  return applyDecorators(
-    UseInterceptors(KakaoInterceptor),
-    Controller('kakao'),
-  );
+  return applyDecorators(UseInterceptors(KakaoInterceptor), Controller('kakao'));
 };
 
-export const KakaoIntent = (path?: KakaoIntentId) => {
+export const KakaoIntent = (path: KakaoIntentId | KakaoFallbackRoute) => {
   return Post(path);
 };
